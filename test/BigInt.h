b13dd9534febbcba19529e158b66164ea59706b2
gaurav61 <gaurav.arora0077@gmail.com>
2020-12-25 18:27:30 +0530

    Added make file to support cross platform builds

---
+++ test/BigInt.h
@@ -0,0 +1,36 @@
+#ifndef BigInt_H
+#define BigInt_H
+#include<string>
+using namespace std;
+class BigInt{
+
+    string var;
+
+    public:
+        BigInt();
+        BigInt(string x);
+        BigInt add(BigInt obj);
+        BigInt sub(BigInt obj);
+        BigInt mul(BigInt obj);
+        BigInt div(long long int y);
+        BigInt mod(long long int y);
+        BigInt abs();
+        string getVal();
+        void operator = (string x);
+        BigInt operator + (BigInt const &obj);
+        BigInt operator - (BigInt const &obj);
+        BigInt operator - ();
+        BigInt operator * (BigInt const &obj);
+        BigInt operator / (long long int y);
+        BigInt operator % (long long int y);
+        static bool isSmaller(string x , string y);
+        static BigInt fact(int y);
+        static BigInt pow(string x, int y);
+        static string subHelper(string x , string y);
+        static string mulHelper(string x , string y);
+        static string divHelper(string x , long long int y);
+        static bool allZeroes(string x);
+        friend ostream& operator<<(ostream& os, BigInt const &obj);
+};
+
+#endif
